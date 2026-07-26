dcd0e21d17c756ffcd0931ce50818d222bf9e01e
OmarsWat1994 <mrmw2929@gmail.com>
2026-07-26 19:21:49 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.voice_our_iraq_app"
-    compileSdk = 34
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     configurations.all {
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.example.voice_our_iraq_app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
