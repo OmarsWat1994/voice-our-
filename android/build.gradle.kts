29b2325771ad8dfcf5b093f53ba7a8e17b3faba5
OmarsWat1994 <mrmw2929@gmail.com>
2026-07-26 17:48:40 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,17 +24,11 @@ tasks.register<Delete>("clean") {
 }
 
 subprojects {
-    plugins.withId("com.android.application") {
-        the<com.android.build.gradle.AppExtension>().apply {
-            compileSdkVersion(33)
-            defaultConfig {
-                targetSdkVersion(33)
+    afterEvaluate { project ->
+        if (project.hasProperty("android")) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(33)
+                defaultConfig {
+                    targetSdkVersion(33)
+                }
             }
-        }
-    }
-    plugins.withId("com.android.library") {
-        the<com.android.build.gradle.LibraryExtension>().apply {
-            compileSdkVersion(33)
-        }
-    }
-}
